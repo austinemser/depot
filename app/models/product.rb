6805bf23242d81180b6ef0be92adf2abfb97edef
Austin Emser <austin.emser@gmail.com>
2012-07-20 15:51:11 -0400

    Starting Chapter 10

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_line_item
+  
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -7,5 +11,14 @@ class Product < ActiveRecord::Base
     with:     %r{\.(gif|jpg|png)$}i,
     message:  'must be a URL for a GIF, JPG or PNG image.'
   }
+  
+  def ensure_not_referenced_by_line_item
+    if line_items.empty?
+      return true
+    else
+      errros.add(:base, 'Line Items Present')
+      return false
+    end
+  end
   
 end
